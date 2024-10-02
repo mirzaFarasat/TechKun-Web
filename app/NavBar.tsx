0f10b701ee42707c63d642515f2d19bc8bdf3e58
navedm1424 <naved.infomover@gmail.com>
2024-10-03 04:52:13 +0530

    Developments.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,34 +1,49 @@
 'use client';
-
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import React from 'react'
 import classNames from 'classnames';
 import { FaBug } from "react-icons/fa";
+import { Button } from '@radix-ui/themes';
+
+
 const NavBar = () => {
     const currentPath = usePathname();
-
     const links = [
-        {lable:'About Us', href:'/about-us'},
-        {lable:'Our Work', href:'/services'},
-        {lable:'Carriers', href:'/'},
-        {lable:'Contact Us', href:'/contact-us'}
-    ]
-  return (
-    <nav className='flex space-x-8 border-b px-8 h-20 bg-slate-200 items-center'>
-        <Link href="/"><FaBug className='hover:text-zinc-500 transition-colors'/></Link>
-        <ul className='space-x-6'>
-            {links.map(link => 
-            <Link 
-            key={link.href} className={classNames({
-                'text-zinc-900': link.href===currentPath,
-                'text-zinc-500': link.href!==currentPath,
-                'hover:text-zinc-800 transition-colors': true
-            })}
-            href={link.href}>{link.lable}</Link>)}
-        </ul>
-    </nav> 
-  )
-}
+        { lable: 'Our Work', href: '/services' },
+        // { lable: 'Carriers', href: '/' },
+        { lable: 'About Us', href: '/about-us' },
+        // { lable: 'Contact Us', href: '/contact-us' }
+    ];
 
-export default NavBar
+    return (
+        <nav className='fixed top-0 shadow border-b w-full h-16 bg-white'>
+            <div className='case-responsive-container h-full flex justify-between items-center'>
+                <div className='flex flex-row items-center space-x-16'>
+                    <div className='flex flex-row items-center space-x-2 text-lg font-semibold'>
+                        <Link className='' href="/">
+                            <FaBug className='hover:text-zinc-500 transition-colors' />
+                        </Link>
+                        <div>Techkun</div>
+                    </div>
+
+                    <ul className='space-x-12'>
+                        {links.map(link =>
+                            <Link key={link.href} className={classNames({
+                                    'text-zinc-900': link.href === currentPath,
+                                    'text-zinc-500': link.href !== currentPath,
+                                    'hover:text-zinc-800 font-semibold transition-colors': true
+                                })}
+                                href={link.href}>
+                                {link.lable}
+                            </Link>)}
+                    </ul>
+                </div>
+
+                <Button size={"3"} color="blue">Contact Us</Button>
+            </div>
+        </nav>
+    )
+};
+
+export default NavBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -31,9 +31,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Theme>
-          <NavBar />       
+          <NavBar />
           <main className=''>{children}</main>
-          <Theme accentColor="iris"></Theme>
         </Theme>
       </body>
     </html>
