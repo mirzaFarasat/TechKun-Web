57c52ac3169801b00b4aa7350e33ef24ea905317
Mirza Farasat <khnuzr@gmail.com>
2024-10-03 03:37:40 +0530

    Some code refinements.

--- app/services/banner.tsx
+++ app/services/banner.tsx
@@ -10,15 +10,13 @@ const quotesImg = {
 
 const Banner = () => {
     return (
-        <div className="bg-cover bg-center h-96 w-full flex justify-start items-center font-bold text-white text-6xl"
+        <div className="bg-cover bg-center h-96 w-full flex justify-center items-center font-bold text-white text-6xl"
             style={quotesImg}>
-            <div className='flex ml-80'>
                 <div>
                     <h1 className=''>Your Vision, Our Expertise:</h1>
                     <h1>Custom Software to</h1>
                     <h1 className=' text-blue-500'>Elevate Your Business</h1>
                 </div>
-            </div>
         </div>
     )
 }
