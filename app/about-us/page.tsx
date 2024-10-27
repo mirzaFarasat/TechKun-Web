5c20da06a62f9a465eec6fdc16237855d41253be
Mirza Farasat <khnuzr@gmail.com>
2024-10-27 06:07:11 +0530

    Changed about us.

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div className="case-responsive-container flex justify-center items-center text-center text-4xl font-bold py-72 bg-primary-50" >
+      <h1><span className='text-primary-700'>We are</span> working on it.</h1>
+      </div>
   )
 }
 
