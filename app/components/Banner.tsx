ebc973d0f488707d11cbffafebe42662b49ee046
Naved M. Usmani <navedm1424@gmail.com>
2025-02-26 03:18:58 +0530

    Resized the banners.

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -2,39 +2,36 @@ import React from "react";
 import Link from "next/link";
 
 const Banner = () => {
-  const bannerImg = {
-    backgroundImage: "url('/Images/landing-page-banner/landing-banner3.jpg')",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    width: "100%",
-    height: "100vh",
-  };
+    const bannerImg = {
+        backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/Images/landing-page-banner/landing-banner3.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+    };
 
-  return (
-    <div
-      className="case-responsive-container relative h-screen bg-cover bg-center"
-      style={bannerImg} 
-    >
-      
-      <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-start px-10 md:px-20 text-left text-white">
-        <h1 className="text-3xl md:text-5xl sm:text-4xl font-bold">
-          <span className="text-primary-400">EMPOWERING INNOVATIONS,</span>
-          <br />
-          DELIVERING EXCELLENCE
-        </h1>
-        <p className="text-white text-lg mt-4">
-          Partnering with medium to large enterprises, we specialize in<br className='hidden md:block'/>
-          catalyzing digital business transformations, delivering agile and<br className='hidden md:block'/>
-          innovative solutions for competitive growth.
-        </p>
-        <button className="mt-6 px-6 py-2 bg-white text-black rounded-lg font-semibold hover:bg-primary-500 hover:text-white">
-          <Link href="/contact-us">Connect with us</Link>
-        </button>
-      </div>
-    </div>
+    return (
+        <div
+            className="relative w-full flex flex-col justify-center items-left"
+            style={bannerImg}
+        >
+            <div className="case-responsive-container py-56 text-left text-white">
+                <h1 className="text-3xl md:text-5xl sm:text-4xl font-bold">
+                    <span className="text-primary-400">EMPOWERING INNOVATIONS,</span>
+                    <br />
+                    DELIVERING EXCELLENCE
+                </h1>
+                <p className="text-white text-lg mt-4">
+                    Partnering with medium to large enterprises, we specialize in<br className='hidden md:block' />
+                    catalyzing digital business transformations, delivering agile and<br className='hidden md:block' />
+                    innovative solutions for competitive growth.
+                </p>
+                <button className="mt-6 px-6 py-2 bg-white text-black rounded-lg font-semibold hover:bg-primary-500 hover:text-white">
+                    <Link href="/contact-us">Connect with us</Link>
+                </button>
+            </div>
+        </div>
 
-  );
+    );
 };
 
 export default Banner;
--- app/services/banner.tsx
+++ app/services/banner.tsx
@@ -2,36 +2,32 @@ import React from "react";
 import Link from "next/link";
 
 const bannerStyle = {
-  backgroundImage: "url('/Images/banner-images/Services banner2.jpg')",
-  backgroundSize: "cover",
-  backgroundPosition: "center",
+    backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/Images/banner-images/Services banner2.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
 };
 
 const Banner = () => {
-  return (
-    <div
-      className="relative h-[30rem] w-full flex flex-col justify-center items-left text-left px-6 sm:px-12"
-      style={bannerStyle}
-    >
-      {/* Overlay for better text readability */}
-      <div className="absolute inset-0 bg-black/50"></div>
-
-      {/* Banner Content */}
-      <div className="case-responsive-container relative z-10 max-w-3xl text-white">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
-        Your Vision, Our Expertise: <br />
-        Custom Software to <br />
-        <span className='text-primary-300'>Elevate Your Business</span>
-        </h1>
-        <p className="mt-4 text-lg sm:text-xl text-gray-200">
-          Custom software solutions tailored to elevate your business.
-        </p>
-        <button className="mt-6 px-6 py-2 bg-white text-black rounded-lg font-semibold hover:bg-primary-500 hover:text-white">
-            <Link href="/contact-us">Connect with us</Link>
-          </button>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className="relative w-full flex flex-col justify-center items-left text-left"
+            style={bannerStyle}
+        >
+            <div className="case-responsive-container py-56 relative text-white">
+                <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
+                    Your Vision, Our Expertise: <br />
+                    Custom Software to <br />
+                    <span className='text-primary-300'>Elevate Your Business</span>
+                </h1>
+                <p className="mt-4 text-lg sm:text-xl text-gray-200">
+                    Custom software solutions tailored to elevate your business.
+                </p>
+                <button className="mt-6 px-6 py-2 bg-white text-black rounded-lg font-semibold hover:bg-primary-500 hover:text-white">
+                    <Link href="/contact-us">Connect with us</Link>
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Banner;
