9f000350e3d8112931bfe370c2afa6f0445c1491
Naved M. Usmani <navedm1424@gmail.com>
2025-11-13 03:08:25 +0530

    Enhance LogoAnimated component with dynamic path generation
    - Refactored `LogoAnimated` to utilize motion values for dynamic path updates.
    - Introduced a new path generation function in `techkun-logo-path.ts` for improved SVG path accuracy.
    - Updated animation logic to allow for smoother transitions and interactions.
    - Removed the web worker implementation for path generation, simplifying the component structure.

--- app/components/LogoAnimated.tsx
+++ app/components/LogoAnimated.tsx
@@ -1,67 +1,158 @@
 'use client';
-import { useEffect, useState } from "react";
+import { PathBuilder } from "@/lib/svg-utils/path";
+import { Point } from "@/lib/svg-utils/svg";
+import { animate, motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect } from "react";
 
-const stringPath = `
-    m 695 490
-    c -11.0457 0 -20 8.9543 -20 20
-    l 0 200
-    c 0 71.797 -58.203 130 -130 130
-    c -71.797 0 -130 -58.203 -130 -130
-    l 0 -360
-    c 0 -25 0 -25 -25 -25
-    l -210 0
-    c -71.797 0 -130 -58.203 -130 -130
-    c 0 -71.797 58.203 -130 130 -130
-    l 215 0
-    c 52.3327 0 100 55.8413 100 115
-    l 0 40
-    c 0 25 0 25 25 25
-    l 290 0
-    c 15 0 22.249 -1.688 35.1305 -15.7902
-    c 21.2237 -20.3863 51.7361 -27.8524 79.9749 -19.569
-    c 28.7053 8.4202 50.5578 31.7885 57.0369 60.9932
-    c 7.6474 34.4708 -7.6038 69.9702 -37.8716 88.1519
-    c -34.7228 20.8578 -79.4692 13.2715 -105.3768 -17.8656
-    c -3.8939 -5.9203 -13.8939 -15.9203 -28.8939 -15.9203
-    l -290 0
-    c -25 0 -25 0 -25 25
-    l 0 360
-    c 0 27.6142 22.3858 50 50 50
-    c 27.6142 0 50 -22.3858 50 -50
-    l 0 -260
-    c 0 -22.0914 17.9086 -40 40 -40
-    l 220 0
-    c 22.0914 0 40 17.9086 40 40
-    c 0 22.0914 -17.9086 40 -40 40
-    z
-    m -305 -245
-    c 25 0 25 0 25 -25
-    l 0 -40
-    c 0 -19.33 -22.3858 -35 -50 -35
-    l -185 0
-    c -27.6142 0 -50 22.3858 -50 50
-    c 0 27.6142 22.3858 50 50 50
-    z
-`;
+interface PathProps {
+    thickness: number;
+    innerSpacing: number;
+    cornerCurveSize: number;
+    bottomDropLength: number;
+
+    upperArmLength: number;
+    lowerArmLength: number;
+    interArmGap: number;
+    loopWidth: number;
+
+    orbEntryAngle: number;
+};
+
+const orbBaseAngleRatio = 4 * Math.atan(18 / 35) / Math.PI;
+
+function pathData(props: PathProps) {
+    const {
+        thickness, innerSpacing,
+        cornerCurveSize,
+        upperArmLength, lowerArmLength,
+        interArmGap,
+        bottomDropLength, loopWidth,
+        orbEntryAngle
+    } = props;
+
+    const origin: Point = Point.of(
+        loopWidth + 5 * innerSpacing / 2 + 3 * thickness + cornerCurveSize,
+        interArmGap + 2 * innerSpacing + 3 * thickness
+    );
+    
+    const pathBuilder = PathBuilder.m(origin)
+        .cSmoothConnector(Point.of(-cornerCurveSize, cornerCurveSize), Math.PI, -Math.PI / 2, 1 / Math.SQRT2)
+        .l(Point.of(0, bottomDropLength - cornerCurveSize))
+        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + innerSpacing / 2), (thickness + innerSpacing / 2)))
+        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + innerSpacing / 2), -(thickness + innerSpacing / 2)))
+        .l(Point.of(0, -(bottomDropLength + thickness + interArmGap - cornerCurveSize)))
+        .cSmoothConnector(Point.of(-cornerCurveSize, -cornerCurveSize), -Math.PI / 2, 0, 1 / Math.SQRT2)
+        .l(Point.of(-(loopWidth - cornerCurveSize), 0))
+        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + innerSpacing / 2), -(thickness + innerSpacing / 2)))
+        .cForCircularArc(Math.PI / 2, Point.of(thickness + innerSpacing / 2, -(thickness + innerSpacing / 2)))
+        .l(Point.of(loopWidth - (50 - 10), 0))
+        .cSmoothConnector(Point.of(50 - 10 + thickness, innerSpacing - cornerCurveSize + thickness - 10), undefined, -Math.PI / 2, 1/2, 1/Math.sqrt(6))
+        .l(Point.of(0, 10))
+        .cSmoothConnector(Point.of(cornerCurveSize, cornerCurveSize), Math.PI / 2, Math.PI, 1 / Math.SQRT2)
+        .l(Point.of(upperArmLength - cornerCurveSize, 0));
+    
+    const arcDivisions = 4;
+    const rotation = (Math.PI - orbEntryAngle * 2) / arcDivisions;
+    
+    const base = thickness * 7 / 16;
+    const height = base * Math.tan(orbEntryAngle * orbBaseAngleRatio);
+    const endcapCircleRadius = (thickness - 2 * height) / 2 / Math.cos(orbEntryAngle);
+    
+    pathBuilder.cSmoothConnector(
+        Point.of(base, height), 0, orbEntryAngle + Math.PI, 1/3
+    );
+    for (let i = 0; i < arcDivisions; i++) {
+        pathBuilder.cForCircularArc(
+            Point.of(
+                -endcapCircleRadius * Math.sin(orbEntryAngle + i * rotation),
+                endcapCircleRadius * Math.cos(orbEntryAngle + i * rotation)
+            ),
+            rotation
+        );
+    }
+    pathBuilder.cSmoothConnector(
+        Point.of(-base, height), undefined, 0, 1/3
+    );Math.sec
+
+    pathBuilder.l(Point.of(-(upperArmLength - cornerCurveSize), 0))
+        .cSmoothConnector(Point.of(-cornerCurveSize, cornerCurveSize), Math.PI, -Math.PI / 2, 1 / Math.SQRT2)
+        .l(Point.of(0, bottomDropLength + thickness + interArmGap - cornerCurveSize))
+        .cForCircularArc(-Math.PI / 2, Point.of(innerSpacing / 2, innerSpacing / 2))
+        .cForCircularArc(-Math.PI / 2, Point.of(innerSpacing / 2, -innerSpacing / 2))
+        .l(Point.of(0, -(bottomDropLength + thickness / 2)))
+        .cForCircularArc(Math.PI / 2, Point.of(thickness / 2, -thickness / 2))
+        .l(Point.of(lowerArmLength, 0))
+        .cForCircularArc(Math.PI / 2, Point.of(thickness / 2, thickness / 2))
+        .cForCircularArc(Math.PI / 2, Point.of(-thickness / 2, thickness / 2))
+        .l(Point.of(-(lowerArmLength - thickness / 2 - cornerCurveSize), 0))
+        .z()
+        .m(Point.of(-(innerSpacing + 2 * cornerCurveSize + 2 * thickness), -(2 * thickness + interArmGap)))
+        .cSmoothConnector(Point.of(cornerCurveSize, -cornerCurveSize), 0, Math.PI / 2, 1 / Math.SQRT2)
+        .cSmoothConnector(Point.of(-50, -(innerSpacing - cornerCurveSize)), undefined, 0, 1/Math.sqrt(6), 1/2)
+        .l(Point.of(-(loopWidth - 50), 0))
+        .cForCircularArc(-Math.PI / 2, Point.of(-innerSpacing / 2, innerSpacing / 2))
+        .cForCircularArc(-Math.PI / 2, Point.of(innerSpacing / 2, innerSpacing / 2))
+        .z();
+
+    return pathBuilder.toString();
+};
+
+const finalProps: PathProps = {
+    thickness: 80,
+    innerSpacing: 100,
+    cornerCurveSize: 25,
+    
+    upperArmLength: 315,
+    lowerArmLength: 220,
+    interArmGap: 85,
+    bottomDropLength: 220,
+    loopWidth: 235,
+
+    orbEntryAngle: -Math.PI / 4
+};
+
+const finalPath = pathData(finalProps);
 
 export default function LogoAnimated() {
-    const [path, setPath] = useState('');
+    const upperArmLength = useMotionValue(0);
+    const orbEntryAngle = useMotionValue(0);
+    const path = useTransform<number, string>(
+        [upperArmLength, orbEntryAngle],
+        value => pathData({
+            ...finalProps,
+            upperArmLength: value[0],
+            orbEntryAngle: value[1]
+        })
+    );
+    async function startAnimation() {
+        await animate(upperArmLength, finalProps.upperArmLength, {
+            duration: 0.5,
+            delay: 1
+        });
+        await animate(orbEntryAngle, finalProps.orbEntryAngle, {
+            duration: 0.5
+        });
+    };
+    async function reverseAnimation() {
+        await animate(orbEntryAngle, 0, {
+            duration: 0.5
+        });
+        await animate(upperArmLength, 0, {
+            duration: 0.5
+        });
+        startAnimation();
+    };
     useEffect(() => {
-        const worker = new Worker(new URL("../../lib/techkun-logo-path.ts", import.meta.url));
-        worker.postMessage(null);
-        worker.onmessage = e => setPath(e.data);
-        return () => worker.terminate();
+        startAnimation();
     }, []);
     // const viewBoxWidth = spiralLength + 4 * breathingRoom + 3 * thickness + startHandLength;
     // const viewBoxHeight = gapBwStartHandAndShaft + 7 * breathingRoom / 2 + 4 * thickness + tailLength;
-    return <div>
-        <svg width={550} height={500} viewBox={`0 0 1100 1000`}>
-            <path d={path}
-                style={{
-                    transition: "d 1000ms"
-                }}
+    return <div onClick={reverseAnimation}>
+        <motion.svg width={550} height={500} viewBox={`0 0 1100 1000`}>
+            <motion.path
+                d={path}
                 fill="#5692d8"
             />
-        </svg>
+        </motion.svg>
     </div>;
 };
--- lib/techkun-logo-path.ts
+++ lib/techkun-logo-path.ts
@@ -1,6 +1,18 @@
 import { PathBuilder } from "./svg-utils/path";
 import { Point } from "./svg-utils/svg";
 
+interface PathProps {
+    thickness: number;
+    innerSpacing: number;
+    cornerCurveSize: number;
+    bottomDropLength: number;
+
+    upperArmLength: number;
+    lowerArmLength: number;
+    interArmGap: number;
+    loopWidth: number;
+};
+
 self.onmessage = function(e) {
     const thickness = 80;
     const innerSpacing = 100;
@@ -11,6 +23,40 @@ self.onmessage = function(e) {
     const interArmGap = 85;
     const bottomDropLength = 220;
     const loopWidth = 235;
+    
+    postMessage([
+        getPathWithoutEndcapCircle({
+            thickness, innerSpacing,
+            cornerCurveSize,
+            upperArmLength: 0, lowerArmLength,
+            interArmGap,
+            bottomDropLength, loopWidth
+        }),
+        getPathWithoutEndcapCircle({
+            thickness, innerSpacing,
+            cornerCurveSize,
+            upperArmLength, lowerArmLength,
+            interArmGap,
+            bottomDropLength, loopWidth
+        }),
+        getFullPathData({
+            thickness, innerSpacing,
+            cornerCurveSize,
+            upperArmLength, lowerArmLength,
+            interArmGap,
+            bottomDropLength, loopWidth
+        })
+    ]);
+};
+
+function getFullPathData(props: PathProps) {
+    const {
+        thickness, innerSpacing,
+        cornerCurveSize,
+        upperArmLength, lowerArmLength,
+        interArmGap,
+        bottomDropLength, loopWidth
+    } = props;
 
     const origin: Point = Point.of(
         loopWidth + 5 * innerSpacing / 2 + 3 * thickness + cornerCurveSize,
@@ -33,47 +79,29 @@ self.onmessage = function(e) {
         .cSmoothConnector(Point.of(cornerCurveSize, cornerCurveSize), Math.PI / 2, Math.PI, 1 / Math.SQRT2)
         .l(Point.of(upperArmLength - cornerCurveSize, 0));
     
-    const initialAngle = Math.PI / 4;
-    const rotation = 3 * Math.PI / 8;
+    const initialAngle = -Math.PI / 4;
+    const arcDivisions = 4;
+    const rotation = 3 * Math.PI / 2 / arcDivisions;
     const endcapCircleRadius = thickness + 2;
     pathBuilder.cSmoothConnector(
         Point.of(
             thickness * 7 / 16,
             -thickness * 9 / 40
-        ), 0, 3 * initialAngle, 1/3
+        ), 0, initialAngle + Math.PI, 1/3
     );
-    pathBuilder.cForCircularArc(
-        Point.of(
-            endcapCircleRadius * Math.cos(initialAngle + 0 * rotation),
-            endcapCircleRadius * Math.sin(initialAngle + 0 * rotation)
-        ),
-        rotation
-    );
-    pathBuilder.cForCircularArc(
-        Point.of(
-            endcapCircleRadius * Math.cos(initialAngle + 1 * rotation),
-            endcapCircleRadius * Math.sin(initialAngle + 1 * rotation)
-        ),
-        rotation
-    );
-    pathBuilder.cForCircularArc(
-        Point.of(
-            endcapCircleRadius * Math.cos(initialAngle + 2 * rotation),
-            endcapCircleRadius * Math.sin(initialAngle + 2 * rotation)
-        ),
-        rotation
-    );
-    pathBuilder.cForCircularArc(
-        Point.of(
-            endcapCircleRadius * Math.cos(initialAngle + 3 * rotation),
-            endcapCircleRadius * Math.sin(initialAngle + 3 * rotation)
-        ),
-        rotation
-    );
+    for (let i = 0; i < arcDivisions; i++) {
+        pathBuilder.cForCircularArc(
+            Point.of(
+                -endcapCircleRadius * Math.sin(initialAngle + i * rotation),
+                endcapCircleRadius * Math.cos(initialAngle + i * rotation)
+            ),
+            rotation
+        );
+    }
     pathBuilder.cSmoothConnector(
         Point.of(
             -thickness * 7 / 16,
-            thickness + (thickness * 9 / 40) - endcapCircleRadius * 2 * Math.sin(Math.PI / 4)
+            thickness + (thickness * 9 / 40) - 2 * endcapCircleRadius * Math.sin(Math.PI / 4)
         ), undefined, 0, 1/3
     );
 
@@ -87,6 +115,7 @@ self.onmessage = function(e) {
         .l(Point.of(lowerArmLength, 0))
         .cForCircularArc(Math.PI / 2, Point.of(thickness / 2, thickness / 2))
         .cForCircularArc(Math.PI / 2, Point.of(-thickness / 2, thickness / 2))
+        .l(Point.of(-lowerArmLength, 0))
         .z()
         .m(Point.of(-(innerSpacing + 2 * cornerCurveSize + 2 * thickness), -(2 * thickness + interArmGap)))
         .cSmoothConnector(Point.of(cornerCurveSize, -cornerCurveSize), 0, Math.PI / 2, 1 / Math.SQRT2)
@@ -95,6 +124,85 @@ self.onmessage = function(e) {
         .cForCircularArc(-Math.PI / 2, Point.of(-innerSpacing / 2, innerSpacing / 2))
         .cForCircularArc(-Math.PI / 2, Point.of(innerSpacing / 2, innerSpacing / 2))
         .z();
+
+    return pathBuilder.toString();
+};
+
+function getPathWithoutEndcapCircle(props: PathProps) {
+    const {
+        thickness, innerSpacing,
+        cornerCurveSize,
+        upperArmLength, lowerArmLength,
+        interArmGap,
+        bottomDropLength, loopWidth
+    } = props;
+
+    const origin: Point = Point.of(
+        loopWidth + 5 * innerSpacing / 2 + 3 * thickness + cornerCurveSize,
+        interArmGap + 2 * innerSpacing + 3 * thickness
+    );
     
-    postMessage(pathBuilder.toString());
+    const pathBuilder = PathBuilder.m(origin)
+        .cSmoothConnector(Point.of(-cornerCurveSize, cornerCurveSize), Math.PI, -Math.PI / 2, 1 / Math.SQRT2)
+        .l(Point.of(0, bottomDropLength - cornerCurveSize))
+        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + innerSpacing / 2), (thickness + innerSpacing / 2)))
+        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + innerSpacing / 2), -(thickness + innerSpacing / 2)))
+        .l(Point.of(0, -(bottomDropLength + thickness + interArmGap - cornerCurveSize)))
+        .cSmoothConnector(Point.of(-cornerCurveSize, -cornerCurveSize), -Math.PI / 2, 0, 1 / Math.SQRT2)
+        .l(Point.of(-(loopWidth - cornerCurveSize), 0))
+        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + innerSpacing / 2), -(thickness + innerSpacing / 2)))
+        .cForCircularArc(Math.PI / 2, Point.of(thickness + innerSpacing / 2, -(thickness + innerSpacing / 2)))
+        .l(Point.of(loopWidth - (50 - 10), 0))
+        .cSmoothConnector(Point.of(50 - 10 + thickness, innerSpacing - cornerCurveSize + thickness - 10), undefined, -Math.PI / 2, 1/2, 1/Math.sqrt(6))
+        .l(Point.of(0, 10))
+        .cSmoothConnector(Point.of(cornerCurveSize, cornerCurveSize), Math.PI / 2, Math.PI, 1 / Math.SQRT2)
+        .l(Point.of(upperArmLength - cornerCurveSize, 0));
+    
+    const initialAngle = 0;
+    const arcDivisions = 4;
+    const rotation = Math.PI / arcDivisions;
+    const endcapCircleRadius = thickness / 2;
+    pathBuilder.cSmoothConnector(
+        Point.of(
+            thickness * 0,
+            -thickness * 0
+        ), 0, initialAngle + Math.PI, 1/3
+    );
+    for (let i = 0; i < arcDivisions; i++) {
+        pathBuilder.cForCircularArc(
+            Point.of(
+                -endcapCircleRadius * Math.sin(initialAngle + i * rotation),
+                endcapCircleRadius * Math.cos(initialAngle + i * rotation)
+            ),
+            rotation
+        );
+    }
+    pathBuilder.cSmoothConnector(
+        Point.of(
+            -thickness * 0,
+            thickness + (thickness * 0) - 2 * endcapCircleRadius * Math.sin(Math.PI / 2)
+        ), undefined, 0, 1/3
+    );
+
+    pathBuilder.l(Point.of(-(upperArmLength - cornerCurveSize), 0))
+        .cSmoothConnector(Point.of(-cornerCurveSize, cornerCurveSize), Math.PI, -Math.PI / 2, 1 / Math.SQRT2)
+        .l(Point.of(0, bottomDropLength + thickness + interArmGap - cornerCurveSize))
+        .cForCircularArc(-Math.PI / 2, Point.of(innerSpacing / 2, innerSpacing / 2))
+        .cForCircularArc(-Math.PI / 2, Point.of(innerSpacing / 2, -innerSpacing / 2))
+        .l(Point.of(0, -(bottomDropLength + thickness / 2)))
+        .cForCircularArc(Math.PI / 2, Point.of(thickness / 2, -thickness / 2))
+        .l(Point.of(lowerArmLength, 0))
+        .cForCircularArc(Math.PI / 2, Point.of(thickness / 2, thickness / 2))
+        .cForCircularArc(Math.PI / 2, Point.of(-thickness / 2, thickness / 2))
+        .l(Point.of(-lowerArmLength, 0))
+        .z()
+        .m(Point.of(-(innerSpacing + 2 * cornerCurveSize + 2 * thickness), -(2 * thickness + interArmGap)))
+        .cSmoothConnector(Point.of(cornerCurveSize, -cornerCurveSize), 0, Math.PI / 2, 1 / Math.SQRT2)
+        .cSmoothConnector(Point.of(-50, -(innerSpacing - cornerCurveSize)), undefined, 0, 1/Math.sqrt(6), 1/2)
+        .l(Point.of(-(loopWidth - 50), 0))
+        .cForCircularArc(-Math.PI / 2, Point.of(-innerSpacing / 2, innerSpacing / 2))
+        .cForCircularArc(-Math.PI / 2, Point.of(innerSpacing / 2, innerSpacing / 2))
+        .z();
+
+    return pathBuilder.toString();
 };
