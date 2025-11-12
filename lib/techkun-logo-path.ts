0c15fcbc3e2ee2b5448a149d5a1d1b2f51592ba9
Naved M. Usmani <navedm1424@gmail.com>
2025-11-12 09:41:34 +0530

    Refactor path generation logic in techkun-logo-path.ts
    - Updated variable names for clarity and consistency.
    - Reorganized path building logic to improve readability and maintainability.
    - Adjusted parameters for circular arcs and connectors to enhance path accuracy.

--- lib/techkun-logo-path.ts
+++ lib/techkun-logo-path.ts
@@ -3,86 +3,97 @@ import { Point } from "./svg-utils/svg";
 
 self.onmessage = function(e) {
     const thickness = 80;
-    const breathingRoom = 100;
-    const cornerCurveRoom = 25;
+    const innerSpacing = 100;
+    const cornerCurveSize = 25;
     
-    const startHandLength = 220;
-    const tailLength = 220;
-    const spiralLength = 235;
-    const shaftLength = 315;
-    const gapBwStartHandAndShaft = 85;
-    
-    const origin: Point = Point.of(spiralLength + 5 * breathingRoom / 2 + 3 * thickness + cornerCurveRoom, gapBwStartHandAndShaft + 2 * breathingRoom + 3 * thickness);
+    const upperArmLength = 315;
+    const lowerArmLength = 220;
+    const interArmGap = 85;
+    const bottomDropLength = 220;
+    const loopWidth = 235;
+
+    const origin: Point = Point.of(
+        loopWidth + 5 * innerSpacing / 2 + 3 * thickness + cornerCurveSize,
+        interArmGap + 2 * innerSpacing + 3 * thickness
+    );
     
     const pathBuilder = PathBuilder.m(origin)
-        .cSmoothConnector(Point.of(-cornerCurveRoom, cornerCurveRoom), Math.PI, -Math.PI / 2, 1 / Math.SQRT2)
-        .l(Point.of(0, tailLength - cornerCurveRoom))
-        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + breathingRoom / 2), (thickness + breathingRoom / 2)))
-        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + breathingRoom / 2), -(thickness + breathingRoom / 2)))
-        .l(Point.of(0, -(tailLength + thickness + gapBwStartHandAndShaft - cornerCurveRoom)))
-        .cSmoothConnector(Point.of(-cornerCurveRoom, -cornerCurveRoom), -Math.PI / 2, 0, 1 / Math.SQRT2)
-        .l(Point.of(-(spiralLength - cornerCurveRoom), 0))
-        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + breathingRoom / 2), -(thickness + breathingRoom / 2)))
-        .cForCircularArc(Math.PI / 2, Point.of(thickness + breathingRoom / 2, -(thickness + breathingRoom / 2)))
-        .l(Point.of(spiralLength - (50 - 10), 0))
-        .cSmoothConnector(Point.of(50 - 10 + thickness, breathingRoom - cornerCurveRoom + thickness - 10), undefined, -Math.PI / 2, 1/2, 1/Math.sqrt(6))
+        .cSmoothConnector(Point.of(-cornerCurveSize, cornerCurveSize), Math.PI, -Math.PI / 2, 1 / Math.SQRT2)
+        .l(Point.of(0, bottomDropLength - cornerCurveSize))
+        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + innerSpacing / 2), (thickness + innerSpacing / 2)))
+        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + innerSpacing / 2), -(thickness + innerSpacing / 2)))
+        .l(Point.of(0, -(bottomDropLength + thickness + interArmGap - cornerCurveSize)))
+        .cSmoothConnector(Point.of(-cornerCurveSize, -cornerCurveSize), -Math.PI / 2, 0, 1 / Math.SQRT2)
+        .l(Point.of(-(loopWidth - cornerCurveSize), 0))
+        .cForCircularArc(Math.PI / 2, Point.of(-(thickness + innerSpacing / 2), -(thickness + innerSpacing / 2)))
+        .cForCircularArc(Math.PI / 2, Point.of(thickness + innerSpacing / 2, -(thickness + innerSpacing / 2)))
+        .l(Point.of(loopWidth - (50 - 10), 0))
+        .cSmoothConnector(Point.of(50 - 10 + thickness, innerSpacing - cornerCurveSize + thickness - 10), undefined, -Math.PI / 2, 1/2, 1/Math.sqrt(6))
         .l(Point.of(0, 10))
-        .cSmoothConnector(Point.of(cornerCurveRoom, cornerCurveRoom), Math.PI / 2, Math.PI, 1 / Math.SQRT2)
-        .l(Point.of(shaftLength - cornerCurveRoom, 0));
+        .cSmoothConnector(Point.of(cornerCurveSize, cornerCurveSize), Math.PI / 2, Math.PI, 1 / Math.SQRT2)
+        .l(Point.of(upperArmLength - cornerCurveSize, 0));
     
     const initialAngle = Math.PI / 4;
     const rotation = 3 * Math.PI / 8;
-    const radius = thickness + 2;
-    pathBuilder.cSmoothConnector(Point.of(thickness * 7 / 16, -thickness * 9 / 40), undefined, 3 * initialAngle, 1/3);
+    const endcapCircleRadius = thickness + 2;
+    pathBuilder.cSmoothConnector(
+        Point.of(
+            thickness * 7 / 16,
+            -thickness * 9 / 40
+        ), 0, 3 * initialAngle, 1/3
+    );
     pathBuilder.cForCircularArc(
         Point.of(
-            radius * Math.cos(initialAngle + 0 * rotation),
-            radius * Math.sin(initialAngle + 0 * rotation)
+            endcapCircleRadius * Math.cos(initialAngle + 0 * rotation),
+            endcapCircleRadius * Math.sin(initialAngle + 0 * rotation)
         ),
         rotation
     );
     pathBuilder.cForCircularArc(
         Point.of(
-            radius * Math.cos(initialAngle + 1 * rotation),
-            radius * Math.sin(initialAngle + 1 * rotation)
+            endcapCircleRadius * Math.cos(initialAngle + 1 * rotation),
+            endcapCircleRadius * Math.sin(initialAngle + 1 * rotation)
         ),
         rotation
     );
     pathBuilder.cForCircularArc(
         Point.of(
-            radius * Math.cos(initialAngle + 2 * rotation),
-            radius * Math.sin(initialAngle + 2 * rotation)
+            endcapCircleRadius * Math.cos(initialAngle + 2 * rotation),
+            endcapCircleRadius * Math.sin(initialAngle + 2 * rotation)
         ),
         rotation
     );
     pathBuilder.cForCircularArc(
         Point.of(
-            radius * Math.cos(initialAngle + 3 * rotation),
-            radius * Math.sin(initialAngle + 3 * rotation)
+            endcapCircleRadius * Math.cos(initialAngle + 3 * rotation),
+            endcapCircleRadius * Math.sin(initialAngle + 3 * rotation)
         ),
         rotation
     );
     pathBuilder.cSmoothConnector(
-        Point.of(-(thickness * 7 / 16), thickness + (thickness * 9 / 40) - radius * 2 * Math.sin(Math.PI / 4)), undefined, 0, 1/3
+        Point.of(
+            -thickness * 7 / 16,
+            thickness + (thickness * 9 / 40) - endcapCircleRadius * 2 * Math.sin(Math.PI / 4)
+        ), undefined, 0, 1/3
     );
-    
-    pathBuilder.l(Point.of(-(shaftLength - cornerCurveRoom), 0))
-        .cSmoothConnector(Point.of(-cornerCurveRoom, cornerCurveRoom), Math.PI, -Math.PI / 2, 1 / Math.SQRT2)
-        .l(Point.of(0, tailLength + thickness + gapBwStartHandAndShaft - cornerCurveRoom))
-        .cForCircularArc(-Math.PI / 2, Point.of(breathingRoom / 2, breathingRoom / 2))
-        .cForCircularArc(-Math.PI / 2, Point.of(breathingRoom / 2, -breathingRoom / 2))
-        .l(Point.of(0, -(tailLength + (thickness / 2))))
-        .cForCircularArc(Math.PI / 2, Point.of((thickness / 2), -(thickness / 2)))
-        .l(Point.of(startHandLength, 0))
-        .cForCircularArc(Math.PI / 2, Point.of((thickness / 2), (thickness / 2)))
-        .cForCircularArc(Math.PI / 2, Point.of(-(thickness / 2), (thickness / 2)))
+
+    pathBuilder.l(Point.of(-(upperArmLength - cornerCurveSize), 0))
+        .cSmoothConnector(Point.of(-cornerCurveSize, cornerCurveSize), Math.PI, -Math.PI / 2, 1 / Math.SQRT2)
+        .l(Point.of(0, bottomDropLength + thickness + interArmGap - cornerCurveSize))
+        .cForCircularArc(-Math.PI / 2, Point.of(innerSpacing / 2, innerSpacing / 2))
+        .cForCircularArc(-Math.PI / 2, Point.of(innerSpacing / 2, -innerSpacing / 2))
+        .l(Point.of(0, -(bottomDropLength + thickness / 2)))
+        .cForCircularArc(Math.PI / 2, Point.of(thickness / 2, -thickness / 2))
+        .l(Point.of(lowerArmLength, 0))
+        .cForCircularArc(Math.PI / 2, Point.of(thickness / 2, thickness / 2))
+        .cForCircularArc(Math.PI / 2, Point.of(-thickness / 2, thickness / 2))
         .z()
-        .m(Point.of(-(breathingRoom + 2 * cornerCurveRoom + 2 * thickness), -(2 * thickness + gapBwStartHandAndShaft)))
-        .cSmoothConnector(Point.of(cornerCurveRoom, -cornerCurveRoom), 0, Math.PI / 2, 1 / Math.SQRT2)
-        .cSmoothConnector(Point.of(-50, -(breathingRoom - cornerCurveRoom)), undefined, 0, 1/Math.sqrt(6), 1/2)
-        .l(Point.of(-(spiralLength - 50), 0))
-        .cForCircularArc(-Math.PI / 2, Point.of(-breathingRoom / 2, breathingRoom / 2))
-        .cForCircularArc(-Math.PI / 2, Point.of(breathingRoom / 2, breathingRoom / 2))
+        .m(Point.of(-(innerSpacing + 2 * cornerCurveSize + 2 * thickness), -(2 * thickness + interArmGap)))
+        .cSmoothConnector(Point.of(cornerCurveSize, -cornerCurveSize), 0, Math.PI / 2, 1 / Math.SQRT2)
+        .cSmoothConnector(Point.of(-50, -(innerSpacing - cornerCurveSize)), undefined, 0, 1/Math.sqrt(6), 1/2)
+        .l(Point.of(-(loopWidth - 50), 0))
+        .cForCircularArc(-Math.PI / 2, Point.of(-innerSpacing / 2, innerSpacing / 2))
+        .cForCircularArc(-Math.PI / 2, Point.of(innerSpacing / 2, innerSpacing / 2))
         .z();
     
     postMessage(pathBuilder.toString());
